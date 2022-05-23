9eea702d498ce39c2c0d529f073e071949ac1ec1
Alex Hethrington <salledelavage@pop-os.localdomain>
2022-05-23 19:22:57 -0400

    ajout de password generator

---
+++ password_generator/main.py
@@ -0,0 +1,16 @@
+from msilib.schema import ListBox
+import tkinter as tk
+import random
+  
+tk.geometry("200x200")
+
+canvas = tk.Canvas(tk.top,bg = "white",height = "200")
+
+length = tk.Text(canvas)
+
+
+canvas.pack()
+
+
+
+tk.mainloop()
