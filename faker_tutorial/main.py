ab411dfbe8e5129b9b1e054be8e0695d09fcbd65
Alex Hethrington <salledelavage@pop-os.localdomain>
2022-05-28 08:59:23 -0400

    tutorial faker

--- faker_tutorial/main.py
+++ faker_tutorial/main.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from faker import Faker
 
 fake = Faker(locale="fr_CA")
@@ -6,7 +7,13 @@ print(fake.name())
 print(fake.address())
 print(fake.text())
 
-numbers = [fake.unique.random_int() for _ in range(500)]
-assert len(numbers) == len(set(numbers))
-
+numbers = [fake.unique.random_int() for _ in range(50)]
+assert len(numbers) == len(set(numbers))    # Vérifie que tout est bien unique dans la liste
 print(numbers)
+
+for _ in range(10):
+    print(fake.job())
+    print(fake.file_path(depth=3, category="video"))
+    print(fake.credit_card_number(), fake.credit_card_expire(), fake.credit_card_security_code())
+    print(fake.bothify(text="???-##-???"))
+
