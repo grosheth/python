c3cfccad671ed75e8da5ee35addd5e1f2623066d
Alex Hethrington <salledelavage@pop-os.localdomain>
2022-06-04 10:28:11 -0400

    rebuild le projet de bot

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -1,77 +1,33 @@
-from curses.panel import bottom_panel
-from email import message
-import os, discord, dotenv, asyncio
-from random import randint
-from discord.ext import tasks, commands
+import os, discord, dotenv
+from discord.ext import commands
 
-# Env variables
+
+bot = commands.Bot(command_prefix="!")
 dotenv.load_dotenv()
 intents = discord.Intents.all() 
-bot = commands.Bot(command_prefix="!", intents=intents)
 
-@bot.event
-async def on_ready():
-	print('Logged in as')
-	print(bot.user.name)
-	print(bot.user.id)
-	print('------')
-	print(f"Chu pra")
+# load files in cogs folder
+for filename in os.listdir("./cogs"):
+	if filename.endswith(".py") and filename != "__init__.py":
+		bot.load_extension(f'cogs.{filename[:-3]}')
 
-@bot.event
-async def on_voice_state_update(member, before, after):
-	channel = bot.get_channel(os.getenv("TXTCHANNEL"))
-	message = f"tyl {member.name}"
-	print("test")
-	if not before.channel:
-		print(f"{member.name} joined {after.channel.name}")
-		await bot.send_message(channel, message)
-	if before.channel and not after.channel:
-		print(f"user left channel")
+async def on_ready(self):
+    print('Logged in as')
+    print(bot.user.name)
+    print('------')
+    print(f"Chu pra")
 
+async def on_message(self, message):
+    if message.author.bot:
+        return
 
-@bot.event
-async def on_member_join(member):
-	channel = bot.get_channel(os.getenv("TXTCHANNEL"))
-	await channel.send(f"Salut {member.display_name} bo bb sucre")
+    if message.content[0] != os.getenv("PREFIX"):
+        return
 
-@bot.command(pass_context=True)
-async def join(ctx):
-    author = ctx.message.author
-    channel = author.voice_channel
-    await bot.join_voice_channel(channel)
+    if message.content == os.getenv("PREFIX"):
+        await message.channel.send(embed=discord.Embed(title="Hello I am, P0u55i5l4y3r. Summon me using '!'", description="Feet adorer, womanizer, Short longsword, can do the drapeau. Arch Ennemy: Nick Gingras. Pioussi is the quest, pioussi is the Graal.", color=0xeeafe6))
+    
+    
 
-
-@bot.event
-async def on_message(message):
-
-	poll = message.content
-	games = {
-		1:"https://garticphone.com/lobby",
-		2:"https://songtrivia2.io/",
-		3:"https://world-geography-games.com/en/flags_world.html",
-		4:"https://www.geoguessr.com/"
-	}
-	memes = {
-		1:"https://www.pornhub.com/categories/hentai"
-	}
-
-	# games
-	if message.content == "gartic":
-		await message.channel.send(games[1])
-	if message.content == "songtrivia":
-		await message.channel.send(games[2])
-	if message.content == "flags":
-		await message.channel.send(games[3])
-	if message.content == "geo":
-		await message.channel.send(games[4])
-	if message.content == "random game":
-		await message.channel.send(games[randint(1, len(games))])
-
-	# memes
-	if message.content == "hey bb veux tu une garette pi ecouter des hentai tranquille?":
-		await message.channel.send(memes[1])
-
-	if message.content == "poll":
-		await message.channel.send(embed=discord.Embed(title="Hey pioussies!", description=poll, color=0xeeafe6))
 
 bot.run(os.getenv("TOKEN"))
